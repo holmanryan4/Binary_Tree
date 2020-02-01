cb9b796fc1c1f53d518d8e9a8c4730e9abf35f5d
unknown <holmanryan4@gmail.com>
2020-02-01 15:15:46 -0600

    i have finished the add method for my tree now i am moving on the the search method

---
+++ BinaryTree/BinaryTree.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class BinaryTree : Node
+    {
+
+
+
+        //Member Variables (HAS A)
+        public Node root;
+        public Node current;
+
+
+        //Constructor
+
+
+        //Member Methods (CAN DO)
+        public void Search()
+        {
+           
+        }
+        public Node Add(Node root, int value)
+        {
+            if (root == null)
+            {
+                root = new Node();
+                root.data = value;
+                Console.WriteLine("The tree root is "+ root);
+            }
+
+            if (value >= root.data)
+            {
+                if (root.LeftChild == null)
+                {
+                    root.LeftChild = new Node();
+                    Console.WriteLine("New leftchild is " + root.LeftChild);
+                }
+            }
+            else
+            {
+                if (value < root.data)
+                {
+                    if (root.RightChild == null)
+                    {
+                        root.RightChild = new Node();
+                        Console.WriteLine("New rightchild is " + root.RightChild);
+
+                    }
+                }
+            }
+            
+            if (current.data > value)
+            {
+              current = current.RightChild;
+                Console.WriteLine("New rightchild is " + root.RightChild);
+            }
+            else if(current.data <= value)
+            {
+                current = current.LeftChild;
+                Console.WriteLine("New leftchild is " + root.LeftChild);
+
+            }
+            return null;
+        }
+        
+    }
+}
---
+++ BinaryTree/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Node
+    {
+
+        //Member Variables (HAS A)
+        public int data;
+        public Node LeftChild;
+        public Node RightChild;
+       
+
+       
+        // public Node value { get; private set; }
+       // public Node value = new Node();
+        //Constructor
+
+        //Member Methods (CAN DO)
+
+
+    }
+}
